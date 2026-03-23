5be36c3f633c48555d8cbda2d202dffd7d8fec05
maimuna03134 <mj9644212@gmail.com>
2026-03-23 21:24:51 +0600

    Implemented Home page banner section with CTA buttons, Added states section with three cards, Built Top Apps section with responsive grid layout

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,75 @@
+import React, { useEffect, useState } from 'react';
+import Banner from '../../components/Banner/Banner';
+import useApps from '../../hooks/useApps';
+import AppCard from '../../components/AppCard/AppCard';
+import Container from '../../components/Container/Container';
+import { Link } from 'react-router';
+import ErrorApps from '../ErrorApps/ErrorApps';
+import ErrorPage from '../ErrorPage/ErrorPage';
+import Loader from '../../components/Loader/Loader';
+
+const Home = () => {
+    const { apps, loading, error } = useApps();
+    const [showLoader, setShowLoader] = useState(true);
+    const featuredApps = apps.slice(0, 8);
+
+    useEffect(() => {
+        const timeOut = setTimeout(() => {
+            setShowLoader(false);
+        }, 500);
+
+        return () => clearTimeout(timeOut)
+    }, []);
+
+    if (error) {
+        return <ErrorPage></ErrorPage>;
+    }
+
+    if (loading || showLoader) {
+        return <Loader></Loader>
+    }
+
+    if (!loading && apps.length === 0) {
+        return <ErrorApps></ErrorApps>
+    }
+
+
+    return (
+        <div>
+            <div>
+                <Banner></Banner>
+            </div>
+
+            <div>
+                <Container>
+                    <div className="text-center py-6 ">
+                        <h1 className="text-4xl font-bold text-[#001931]">
+                            Trending Apps
+                        </h1>
+                        <p className="py-3 text-[#627382] text-sm">
+                            Explore All Trending Apps on the Market developed by us
+                        </p>
+                    </div>
+
+                    <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 ">
+                        {featuredApps.map((app) => (
+                            <AppCard key={app.id} app={app}></AppCard>
+                        ))}
+                    </div>
+
+
+                    <div className="mt-8 flex justify-center">
+                        <Link
+                            className="btn btn-outline bg-gradient-to-br from-[#632EE3] to-[#9F62F2] text-white font-bold"
+                            to="/apps"
+                        >
+                            Show All
+                        </Link>
+                    </div>
+                </Container>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
